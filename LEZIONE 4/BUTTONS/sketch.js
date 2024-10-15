a119dde2965d731893479a1bb4555a4410512427
zjncoo <151642330+zjncoo@users.noreply.github.com>
2024-10-15 12:43:55 +0200

    aggiunta bottone

---
+++ LEZIONE 4/BUTTONS/sketch.js
@@ -0,0 +1,28 @@
+let button;
+let elem;
+let dice = 0;
+
+function setup() {
+  elem = createElement( "h2", "Keep rolling");
+  elem.position(0, 0);
+  createCanvas(400, 400);
+  button = createButton("roll the dice");
+  button.position(200, 200);
+  button.mousePressed(rollDice);
+}
+
+function draw() {
+  background(220);
+  stroke("white");
+  strokeWeight(2);
+  fill("white");
+  textSize(30);
+  text("Dice value "+ dice, 5, 300);
+}
+
+function rollDice(){
+  dice = random(1,6);
+  //vogliamo solo numeri interi
+  dice = floor(dice);
+  // dice = ceil(dice);
+}
