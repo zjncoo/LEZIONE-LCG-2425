42b115559c1eed16551451ad1677b61f6677e83a
zjncoo <151642330+zjncoo@users.noreply.github.com>
2024-10-15 10:16:19 +0200

    LEZIONE 2
    aggiunto lezione 4 con translation

---
+++ LEZIONE 4/sketch.js
@@ -0,0 +1,17 @@
+function setup() {
+  createCanvas(400, 400);
+  rectMode(CENTER);
+}
+
+function draw() {
+  background(220);
+  strokeWeight(1);
+  fill("orange");
+  translate(mouseX,mouseY);
+  rect(50, 50, 100, 50);
+  //--> 50+100, 50+100;
+  fill("purple");
+  rect(0,0,200,30);
+  strokeWeight(5);
+  point(50,50);
+}
